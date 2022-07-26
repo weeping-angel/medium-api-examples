f40aa90e030e70483cb0ed06a19c5e7e686e3fc0
Sanskar Jain <sanskarjain1997.19@gmail.com>
2022-07-26 17:50:24 +0530

    updated get_user_id

--- python/get_user_id.py
+++ python/get_user_id.py
@@ -1,13 +1,21 @@
+#-- title: Get User ID
+#-- description: Code to retrieve user_id from username
+#-- tags: python, medium_api, medium_api_py
+
+
 # Import libraries
 import os
 from medium_api import Medium
 
+#%%
 # Get RAPIDAPI_KEY from the environment
 api_key = os.getenv('RAPIDAPI_KEY')
 
+#%%
 # Create a `Medium` Object
 medium = Medium(api_key)
 
+#%%
 # Get the `User` Object using "username" and print ID
 user = medium.user(username="nishu-jain")
 print(user.user_id)
