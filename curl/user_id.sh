db6918f3c793aa0c499677a5ae301b18577abafe
Sanskar Jain <sanskarjain1997.19@gmail.com>
2022-08-10 12:46:12 +0530

    added metadata in user_id.sh

--- curl/user_id.sh
+++ curl/user_id.sh
@@ -1,1 +1,5 @@
+#-- title: User's ID
+#-- description: Shell command to retrieve user_id from username
+#-- tags: shell, medium_api, medium_api_curl
+
 curl -H "x-rapidapi-key: $RAPIDAPI_KEY" https://medium2.p.rapidapi.com/user/id_for/nishu-jain
