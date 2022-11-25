72039fa0bac02e6bec59e7625b41aeecbb9d3991
Sanskar Jain <sanskarjain1997.19@gmail.com>
2022-11-25 12:52:10 +0530

    added related tags #curl

---
+++ curl/related_tags.sh
@@ -0,0 +1,6 @@
+#-- title: Related Tags
+#-- description: Shell command to retrieve related tags for the given tag.
+#-- tags: shell, medium_api, medium_api_curl
+
+curl --header "x-rapidapi-key: $RAPIDAPI_KEY" \
+    https://medium2.p.rapidapi.com/related_tags/blockchain
