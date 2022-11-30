33387407105a5d5cd14eb1356e7558ef2674a5b3
Sanskar Jain <sanskarjain1997.19@gmail.com>
2022-11-30 13:18:18 +0530

    added new endpoint - publication_id

---
+++ curl/publication_id.sh
@@ -0,0 +1,6 @@
+#-- title: Publication's ID
+#-- description: Shell command to retrieve publication_id for the given publication_slug
+#-- tags: shell, medium_api, medium_api_curl
+
+curl --header "x-rapidapi-key: $RAPIDAPI_KEY" \
+    https://medium2.p.rapidapi.com/publication/id_for/towards-data-science
