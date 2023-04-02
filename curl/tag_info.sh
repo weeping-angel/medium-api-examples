d1b05088e09ba6316cf3e8096182323eb541dd3a
Sanskar Jain <sanskarjain1997.19@gmail.com>
2023-04-03 04:06:38 +0530

    added new endpoint - tag_info

---
+++ curl/tag_info.sh
@@ -0,0 +1,6 @@
+#-- title: Tag Info
+#-- description: Shell command to retrieve tag-related information
+#-- tags: shell, medium_api, medium_api_curl
+
+curl --header "x-rapidapi-key: $RAPIDAPI_KEY" \
+    https://medium2.p.rapidapi.com/tag/artificial-intelligence
