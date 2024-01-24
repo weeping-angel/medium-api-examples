e345373e690fc57298d9d66ce885500360fe25a4
Sanskar Jain <sanskarjain1997.19@gmail.com>
2024-01-24 23:29:59 +0530

    fixed output

--- curl/list_responses.sh
+++ curl/list_responses.sh
@@ -3,4 +3,4 @@
 #-- tags: shell, medium_api, medium_api_curl
 
 curl --header "x-rapidapi-key: $RAPIDAPI_KEY" \
-    "https://medium2.p.rapidapi.com/list/38f9e0f9bea6/responses"
+    "https://medium2.p.rapidapi.com/list/3d8f744f5370/responses"
