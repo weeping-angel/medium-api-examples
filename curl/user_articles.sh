e1f851b4c0d7ea2b60439ebc67031f4342b4f6ab
Sanskar Jain <sanskarjain1997.19@gmail.com>
2024-08-07 11:28:02 +0530

    limited results to 500, added next query parameter

--- curl/user_articles.sh
+++ curl/user_articles.sh
@@ -3,4 +3,4 @@
 #-- tags: shell, medium_api, medium_api_curl
 
 curl --header "x-rapidapi-key: $RAPIDAPI_KEY" \
-    "https://medium2.p.rapidapi.com/user/1985b61817c3/articles"
+    "https://medium2.p.rapidapi.com/user/e680fcaf274b/articles?next=1691502178198"
