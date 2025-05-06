8f24f73f8a2345b1b2b2c1825fed0f1f7053ef46
Sanskar Jain <sanskarjain1997.19@gmail.com>
2025-05-06 20:24:04 +0530

    user_following: added pagination

--- curl/user_following.sh
+++ curl/user_following.sh
@@ -3,4 +3,4 @@
 #-- tags: shell, medium_api, medium_api_curl
 
 curl --header "x-rapidapi-key: $RAPIDAPI_KEY" \
-    "https://medium2.p.rapidapi.com/user/14d5c41e0264/following?count=10"
+    "https://medium2.p.rapidapi.com/user/14d5c41e0264/following"
